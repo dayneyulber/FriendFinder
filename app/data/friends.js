f26db9c066bbe8f5c467b7f1a0a074a2126816b0
dayne beronque <dayneyulber@gmail.com>
2019-10-14 21:10:29 -0500

    added friends + setup home html

---
+++ app/data/friends.js
@@ -0,0 +1,130 @@
+var friends = [
+    {
+      "name": "Lionel Messi",
+      "photo": "https://media.gettyimages.com/photos/lionel-messi-of-argentina-poses-for-a-portrait-during-the-official-picture-id972635152?s=2048x2048",
+      "scores": [
+        2,
+        4,
+        1,
+        2,
+        2,
+        5,
+        3,
+        2,
+        3,
+        1
+      ]
+    },
+    {
+      "name": "Cristiano Ronaldo",
+      "photo": "https://media.gettyimages.com/photos/cristiano-ronaldo-of-portugal-poses-for-a-portrait-during-the-fifa-picture-id971158884?s=2048x2048",
+      "scores": [
+        2,
+        4,
+        1,
+        2,
+        4,
+        1,
+        2,
+        5,
+        3,
+        2
+      ]
+    },
+    {
+      "name": "Neymar",
+      "photo": "https://media.gettyimages.com/photos/neymar-of-brazil-poses-for-a-portrait-during-the-official-fifa-world-picture-id973385564?s=2048x2048",
+      "scores": [
+        4,
+        2,
+        1,
+        5,
+        5,
+        2,
+        3,
+        4,
+        5,
+        2
+      ]
+    },
+    {
+      "name": "Eden Hazard",
+      "photo": "https://media.gettyimages.com/photos/eden-hazard-of-belgium-poses-for-a-portrait-during-the-official-fifa-picture-id974275108?s=2048x2048",
+      "scores": [
+        2,
+        3,
+        4,
+        4,
+        1,
+        2,
+        2,
+        2,
+        3,
+        5
+      ]
+    },
+    {
+      "name": "Kylian Mbappe",
+      "photo": "https://media.gettyimages.com/photos/kylian-mbappe-of-france-poses-for-a-portrait-during-the-official-fifa-picture-id971564092?s=2048x2048",
+      "scores": [
+        1,
+        2,
+        3,
+        4,
+        5,
+        4,
+        3,
+        2,
+        1,
+        5
+      ]
+    },
+    {
+      "name": "Raheem Sterling",
+      "photo": "https://media.gettyimages.com/photos/raheem-sterling-of-england-poses-for-a-portrait-during-the-official-picture-id973392600?s=2048x2048",
+      "scores": [
+        5,
+        4,
+        3,
+        2,
+        1,
+        2,
+        3,
+        4,
+        5,
+        4
+      ]
+    },
+    {
+      "name": "Sergio Ramos",
+      "photo": "https://media.gettyimages.com/photos/captain-sergio-ramos-of-spain-poses-for-a-portrait-during-the-fifa-picture-id969773610?s=2048x2048",
+      "scores": [
+        3,
+        2,
+        1,
+        3,
+        2,
+        1,
+        3,
+        2,
+        1,
+        3
+      ]
+    },
+    {
+      "name": "Antoine Griezmann",
+      "photo": "https://media.gettyimages.com/photos/antoine-griezmann-of-france-poses-for-a-portrait-during-the-official-picture-id972039626?s=2048x2048",
+      "scores": [
+        3,
+        4,
+        5,
+        4,
+        3,
+        4,
+        5,
+        4,
+        3,
+        1
+      ]
+    }
+  ]
