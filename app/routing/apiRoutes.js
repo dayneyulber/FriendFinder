7493b4c9e430d6fa7b5e729bb6e21481ceb7d478
dayne beronque <dayneyulber@gmail.com>
2019-10-14 21:13:03 -0500

    setup routes

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,17 @@
+var friendData = require("../data/friends");
+
+var express = require("express");
+var app = express();
+app.use(express.static("public"));
+
+module.exports = function (app) {
+
+app.get("/api/friends", function (req, res) {
+    res.json(friendData);
+});
+
+
+app.post("/api/friends", function(req, res) {
+      friendData.push(req.body);
+      res.json(true);
+    })
